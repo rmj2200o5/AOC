f93b21ab1dbd393d104af439dd3b298ad565417d
rmj2200o5 <85568704+rmj2200o5@users.noreply.github.com>
2022-07-23 09:15:29 -0500

    Update day07.py

--- 2015/day07.py
+++ 2015/day07.py
@@ -3,6 +3,7 @@ with open("C:/Users/Robert J/VS Code/PythonWorks/AdventOfCode/2015/day07in.txt")
 paths = {x.split(" -> ")[1]:x.split(" -> ")[0] for x in data}
 copy = {x:paths.get(x) for x in paths}
 # Part 1
+@lru_cache(len(paths))
 def getSignal(w):
     if w.isdigit():
         return int(w)
@@ -11,34 +12,28 @@ def getSignal(w):
         return wire
     if "AND" in wire:
         w1,w2 = wire.split(" AND ")
-        paths[w] = getSignal(w1) & getSignal(w2)
-        return paths.get(w)
+        return getSignal(w1) & getSignal(w2)
     elif "OR" in wire:
         w1,w2 = wire.split(" OR ")
-        paths[w] = getSignal(w1) | getSignal(w2)
-        return paths.get(w)
+        return getSignal(w1) | getSignal(w2)
     elif "LSHIFT" in wire:
         w1,x = wire.split(" LSHIFT ")
-        paths[w] = getSignal(w1)<<int(x)
-        return paths.get(w)
+        return getSignal(w1)<<int(x)
     elif "RSHIFT" in wire:
         w1,x = wire.split(" RSHIFT ")
-        paths[w] = getSignal(w1)>>int(x)
-        return paths.get(w)
+        return getSignal(w1)>>int(x)
     elif "NOT" in wire:
         w1 = wire.split(" ")[1]
         xo = str(bin(getSignal(w1)))[2:]
         xo = "0"*(16-len(xo))+xo
         xi = "".join(["1" if i == "0" else "0" for i in xo ])
-        paths[w] = int(xi,2)
-        return paths.get(w)
+        return int(xi,2)
     else:
-        paths[w] = getSignal(wire)
-        return paths.get(w)
+        return getSignal(wire)
 
 part1Signal = getSignal("a")
 print(part1Signal)
-
+getSignal.cache_clear()
 # Part 2
 paths = copy
 paths["b"] = part1Signal
