edc623daa9b01ae493b929ebac9c981bec6b3fb1
rmj2200o5 <85568704+rmj2200o5@users.noreply.github.com>
2022-09-17 11:40:17 -0500

    Update day25.py

--- 2015/day25.py
+++ 2015/day25.py
@@ -1,19 +1,3 @@
-# Can I use functools reduce to run the multiplication, division, and modulous on all items?
-
-
-#write formula to derive index from position. use position to tell needed length of list
-#   potential storage issues? List may be too long
-# rows and columns are additive sequences
-# 
-from functools import reduce
-from operator import mul
-
-# arr = [20151125.0,0.0,0.0]
-# ansr = [20151125]
-# arr2 = reduce(lambda a,b : (a*252533)%33554393,arr)
-# print(arr2)
-# print((arr[0]*252533)%33554393)
-
 #Enter the code at row 2978, column 3083.
 def getCount(row, column):
 	return sum(range(row + column - 1)) + column
